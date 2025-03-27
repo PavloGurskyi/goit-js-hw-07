6e0307d93eaf5e71d5d2f2546ab0335b45187c6f
Pavlo Gurskiy <pavlo.gurskiy@gmail.com>
2025-03-27 20:17:11 +0200

    main

--- js/task-1.js
+++ js/task-1.js
@@ -1,30 +1,20 @@
-const customer = {
-  username: 'Mango',
-  balance: 24000,
-  discount: 0.1,
-  orders: ['Burger', 'Pizza', 'Salad'],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const cats = document.querySelectorAll('li.item');
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+console.log(`Number of categories: ${cats.length}`);
+
+// const catsNumber = () => {
+//   console.log(`Number of categories: ${cats.length}`);
+// };
+
+// catsNumber();
+
+// const categories = document.querySelector('#categories');
+
+// console.log(`Number of categories: ${categories.childElementCount}`);
+
+// const items = document.querySelectorAll('.item');
+
+// items.forEach(item => {
+//   console.log(`Category: ${item.firstElementChild.textContent}`);
+//   console.log(`Elements: ${item.lastElementChild.childElementCount}`);
+// });
