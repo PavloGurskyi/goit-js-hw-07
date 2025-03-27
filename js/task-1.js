79faae2618cadd2051fe6a9eb1cee78d49b56436
Pavlo Gurskiy <pavlo.gurskiy@gmail.com>
2025-03-27 21:34:57 +0200

    task-1

--- js/task-1.js
+++ js/task-1.js
@@ -2,19 +2,9 @@ const cats = document.querySelectorAll('li.item');
 
 console.log(`Number of categories: ${cats.length}`);
 
-// const catsNumber = () => {
-//   console.log(`Number of categories: ${cats.length}`);
-// };
+const items = document.querySelectorAll('.item');
 
-// catsNumber();
-
-// const categories = document.querySelector('#categories');
-
-// console.log(`Number of categories: ${categories.childElementCount}`);
-
-// const items = document.querySelectorAll('.item');
-
-// items.forEach(item => {
-//   console.log(`Category: ${item.firstElementChild.textContent}`);
-//   console.log(`Elements: ${item.lastElementChild.childElementCount}`);
-// });
+items.forEach(item => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.childElementCount}`);
+});
